f76a376da2f20f5e3f7c2dd8695806c7749c30ba
Chris Arlt <c.arlt@unexpected.de>
2019-10-21 16:23:37 +0200

    * Bugfixes for the SendToFtrack deployment

--- deploy_source/SendToFtrackConnect/Program.cs
+++ deploy_source/SendToFtrackConnect/Program.cs
@@ -85,7 +85,7 @@ namespace SendToFtrackConnect
             foreach (FileInfo file in files)
             {
                 string temppath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(temppath, false);
+                file.CopyTo(temppath, true);
             }
 
             // If copying subdirectories, copy them and their contents to new location.
