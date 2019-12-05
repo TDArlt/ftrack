a02aa62aec6e470ad94c08f0e4cecb44cb5b61f0
Chris Arlt <c.arlt@unexpected.de>
2019-12-05 16:17:16 +0100

    + Implemented the first version

--- ftrack-connect/export-gantt-chart/hook/action.py
+++ ftrack-connect/export-gantt-chart/hook/action.py
@@ -8,6 +8,9 @@ import argparse
 import json
 import tempfile
 import os
+import datetime
+import calendar
+from types import NoneType
 
 import ftrack_api
 
@@ -43,6 +46,25 @@ def get_filter_string(entity_ids):
     return ', '.join(
         '"{0}"'.format(entity_id) for entity_id in entity_ids
     )
+
+def extract_start_date(taskObject):
+    try:
+        if (not(type(taskObject['start_date']) is NoneType)):
+            return datetime.datetime(
+                taskObject['start_date'].year,
+                taskObject['start_date'].month,
+                taskObject['start_date'].day,
+                taskObject['start_date'].hour,
+                taskObject['start_date'].minute)
+        else:
+            return datetime.datetime(2000, 1, 1)
+    except:
+        return datetime.datetime(2000, 1, 1)
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
+
 
 class unexCreateGanttChartAction(BaseAction):
     '''This is the action for creating a Gantt Chart'''
@@ -166,27 +188,14 @@ class unexCreateGanttChartAction(BaseAction):
         session.commit()
 
         try:
-            # TODO: Encode all the information to a nicely formatted chart
+            # TODO: Selection form before starting job:
+            #  - Select, if we like to show assignee and status as well (which might not be a good idea for clients)
+            #  - Select the scaling width (to create a scrollable page)
 
-
-            # TODO: Work out, if we have a project or entities here.
-            # For a project, we need to get all the tasks and events.
-            # For entities like tasks, we just collect their information in a chart
-
-            # After all, we have a list of a lot of entities here, we like to put into the chart
-
-            # Sort the entities by start date
-
-            # Get first start and last end date
-
-            # Generate table
-
-            # Generate bars for each event
-
-            # TODO: What about custom information (assignee, status)?
-            # TODO: What about connections between tasks?
-            # TODO: What about scaling of the chart?
-            # TODO: What about custom order and hierarchy on the name handside?
+            # TODO: What about additional information (assignee, status)?
+            # TODO: What about scaling of the chart? => Might just be a css-property on "main"-div (but we need to scale the marks as well!)
+            # TODO: What about connections between tasks? => Show arrows?
+            # TODO: What about hierarchy (sort and show parent-grouping?)
 
 
     
@@ -195,22 +204,304 @@ class unexCreateGanttChartAction(BaseAction):
 
             for entity in entities:
                 oneObject = getRealEntityFromTypedContext(session, entity)
-                realEntities.append(oneObject)
 
-                printString += "\t- {0} (Type: {1}), due date: {2}\n".format(oneObject['name'], type(oneObject).__name__, oneObject['end_date'])
+                # Handling projects: We will need to get all the tasks of the project
+                if type(oneObject).__name__ == "Project":
+                    tasks = session.query('select id from Task where project.name is "' + oneObject['name'] + '"')
+                    realEntities.extend(tasks)
+                    milestones = session.query('select id from Milestone where project.name is "' + oneObject['name'] + '"')
+                    realEntities.extend(milestones)
+                else:
+                    realEntities.append(oneObject)
             
-            filecontent = "You selected:\n{0}" .format(printString)
+            realEntities = sorted(realEntities, key=extract_start_date)
+
+            # Get min and max dates
+            minDate = datetime.datetime(3000, 1, 1)
+            maxDate = datetime.datetime(2000, 1, 1)
+            for task in realEntities:
+                if (task['start_date'] != None):
+                    tStartDate = datetime.datetime(task['start_date'].year, task['start_date'].month, task['start_date'].day, task['start_date'].hour, task['start_date'].minute)
+                    if (tStartDate < minDate):
+                        minDate = tStartDate
+                if (task['end_date'] != None):
+                    tEndDate = datetime.datetime(task['end_date'].year, task['end_date'].month, task['end_date'].day, task['end_date'].hour, task['end_date'].minute)
+                    if (tEndDate > maxDate):
+                        maxDate = tEndDate
+            daycount = (maxDate - minDate).days
 
 
 
 
-            # TODO: Export correct file type
+            # Generate HTML file
 
+            # CSS
+            cssStyle = '''
+            body {
+                font-family: 'Roboto', sans-serif;
+                padding: 0pt;
+                margin: 0pt;
+            }
+
+            .main {
+                position: relative;
+                margin: 5pt;
+                padding: 30pt 0% 20pt 0%;
+                background-color: #ddd;
+                border-radius: 2pt;
+                z-index: -20;
+            }
+
+            .weekend_mark {
+                position: absolute;
+                top: 0%;
+                margin: 0%;
+                padding: 0%;
+                background-color: #ccc;
+                height: 100%;
+                z-index: -10;
+                box-sizing: border-box;
+            }
+            .week_mark, .month_mark {
+                position: absolute;
+                top: 0%;
+                margin: 0%;
+                padding: 0%;
+                padding-left: 4pt;
+                height: 10pt;
+                border-left: 1pt #888 solid;
+                height: 100%;
+                font-size: 9pt;
+                font-weight: bold;
+                box-sizing: border-box;
+            }
+
+            .task {
+                margin: 0pt 0pt 2pt 0pt;
+                padding: 5pt;
+                height: 38pt;
+                position: relative;
+                border: 1pt solid #000;
+                border-radius: 2pt;
+                box-sizing: border-box;
+            }
+
+            .task .name {
+                font-size: 9pt;
+                font-weight: bold;
+            }
+
+            .task .start {
+                position: absolute;
+                left: 5pt;
+                bottom: 0pt;
+                height: 10pt;
+                font-size: 7pt;
+            }
+            .task .end {
+                position: absolute;
+                right: 5pt;
+                bottom: 0pt;
+                height: 10pt;
+                font-size: 7pt;
+            }
+
+            .task .type {
+                font-size: 7pt;
+            }
+
+            .milestone {
+                margin: 0pt 0pt 2pt 0pt;
+                padding: 5pt;
+                height: 28pt;
+                position: relative;
+                border-left: solid 2pt #f00;
+                box-sizing: border-box;
+            }
+
+            .milestone .caption {
+                font-size: 9pt;
+                font-weight: bold;
+            }
+            .milestone .end {
+                position: absolute;
+                left: 5pt;
+                bottom: 0pt;
+                height: 10pt;
+                font-size: 7pt;
+            }
+
+            
+            .milestone_bar {
+                position: absolute;
+                width: 2pt;
+                background-color: #f00;
+                top: 10pt;
+                bottom: 0pt;
+            }
+
+
+            '''
+
+            # General beginning
+
+            filecontent = '''
+            <html>
+                <head>
+                    <title>Gantt Chart</title>
+                    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
+                    <style>''' + cssStyle + '''</style>
+                </head>
+                <body>
+                <div class="main">
+                    <div id="marks">
+            '''
+
+            # Write markers for weeks & months
+            for single_date in daterange(minDate, maxDate):
+                tLeft = ((single_date - minDate).days / float(daycount)) * 100.0
+
+                if (single_date.weekday() == 5):
+                    tLength = (2.0 / float(daycount)) * 100.0
+                    filecontent += '''
+                        <div class="weekend_mark" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%;"></div>
+                    '''
+                # if we have less than two months in our overview, mark each week beginning
+                if (daycount < 60 and single_date.weekday() == 0):
+                    tLength = (7.0 / float(daycount)) * 100.0
+                    filecontent += '''
+                        <div class="week_mark" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%;">
+                            <div class="caption">''' + single_date.strftime("%m/%d") + '''</div>
+                        </div>
+                    '''
+                elif (daycount > 60 and single_date.day == 1):
+                    # Otherwise, mark first day of month
+                    tLength = (calendar.monthrange(single_date.year, single_date.month)[1] / float(daycount)) * 100.0
+                    filecontent += '''
+                        <div class="month_mark" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%;">
+                            <div class="caption">''' + single_date.strftime("%b %Y") + '''</div>
+                        </div>
+                    '''
+
+            
+            filecontent += '''
+                </div>
+                '''
+            
+            tasksprint = ""
+            milestonesprint = ""
+
+            # Write all tasks
+            for task in realEntities:
+                if (type(task).__name__ == "Task"):
+                    if (task['start_date'] != None and task['end_date'] != None):
+                        # Task with defined dates: Calculate ranges
+                        tStartDate = datetime.datetime(task['start_date'].year, task['start_date'].month, task['start_date'].day, task['start_date'].hour, task['start_date'].minute)
+                        tEndDate = datetime.datetime(task['end_date'].year, task['end_date'].month, task['end_date'].day, task['end_date'].hour, task['end_date'].minute)
+
+                        tLeft = ((tStartDate - minDate).days / float(daycount)) * 100.0
+                        tLength = ((tEndDate - tStartDate).days / float(daycount)) * 100.0
+                    
+
+                        tasksprint += '''
+                            <div class="task" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%; background-color: ''' + task['type']['color'] + '''C0;">
+                                <div class="name">''' + task['name'] + '''</div>
+                                <div class="start">''' + task['start_date'].strftime("%Y/%m/%d") + '''</div>
+                                <div class="end">''' + task['end_date'].strftime("%Y/%m/%d") + '''</div>
+                                <div class="type">''' + task['type']['name'] + '''</div>
+                            </div>
+                        '''
+                    else:
+                        # Task without defined dates
+                        
+                        if (task['end_date'] != None):
+                            # Task with an end date, but no start date
+                            tEndDate = datetime.datetime(task['end_date'].year, task['end_date'].month, task['end_date'].day, task['end_date'].hour, task['end_date'].minute)
+                            tLeft = ((tEndDate - minDate).days / float(daycount)) * 100.0
+                            tLength = 15
+
+                            if (tLeft - tLength < 0):
+                                tLength = tLeft
+                                tLeft = 0
+                            else:
+                                tLeft = tLeft - tLength
+
+                            tasksprint += '''
+                            <div class="task" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%; background-color: ''' + task['type']['color'] + '''C0; border-left: 0pt;">
+                                <div class="name">''' + task['name'] + '''</div>
+                                <div class="end">''' + task['end_date'].strftime("%Y/%m/%d") + '''</div>
+                                <div class="type">''' + task['type']['name'] + '''</div>
+                            </div>
+                            '''
+                        elif (task['start_date'] != None):
+                            # Task with an start date, but no end date
+                            tStartDate = datetime.datetime(task['start_date'].year, task['start_date'].month, task['start_date'].day, task['start_date'].hour, task['start_date'].minute)
+                            tLeft = ((tStartDate - minDate).days / float(daycount)) * 100.0
+                            tLength = 15
+
+                            if (tLeft + tLength > 100):
+                                tLength = 100 - tLeft
+
+                            tasksprint += '''
+                            <div class="task" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%; background-color: ''' + task['type']['color'] + '''C0; border-right: 0pt;">
+                                <div class="name">''' + task['name'] + '''</div>
+                                <div class="start">''' + task['start_date'].strftime("%Y/%m/%d") + '''</div>
+                                <div class="type">''' + task['type']['name'] + '''</div>
+                            </div>
+                            '''
+                        else:
+                            tLeft = 0
+                            tLength = 100
+
+                            tasksprint += '''
+                            <div class="task" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%; background-color: ''' + task['type']['color'] + '''C0;">
+                                <div class="name">''' + task['name'] + '''</div>
+                                <div class="type">''' + task['type']['name'] + '''</div>
+                            </div>
+                            '''
+
+                elif (type(task).__name__ == "Milestone" and task['end_date'] != None):
+                    # Handling milestones (but only if they have a date)
+
+                    tStartDate = datetime.datetime(task['end_date'].year, task['end_date'].month, task['end_date'].day, task['end_date'].hour, task['end_date'].minute)
+                    tLeft = ((tStartDate - minDate).days / float(daycount)) * 100.0
+                    tLength = 15
+
+                    if (tLeft + tLength > 100):
+                        tLength = 100 - tLeft
+
+                    milestonesprint += '''
+                            <div class="milestone" style="left: ''' + str(tLeft) + '''%; width: ''' + str(tLength) + '''%; background-color: ''' + task['type']['color'] + '''C0;">
+                                <div class="caption">''' + task['name'] + '''</div>
+                                <div class="end">''' + task['end_date'].strftime("%Y/%m/%d") + '''</div>
+                            </div>
+                            <div class="milestone_bar" style="left: ''' + str(tLeft) + '''%;"></div>
+                            '''
+                    
+            # Apply
+            
+            filecontent += '''
+                <div id="milestones">
+                    ''' + milestonesprint + '''
+                </div>
+                <div id="tasks">
+                    ''' + tasksprint + '''
+                </div>
+                '''
+
+
+            # General ending
+
+            filecontent += '''
+                    </div>
+                </div>
+            </html>
+            '''
 
             # Generate unique temp file name
             file_path = tempfile.NamedTemporaryFile(
-                prefix='example_collection', 
-                suffix='.txt', 
+                prefix='gantt_export_', 
+                suffix='.html', 
                 delete=False
             ).name
 
@@ -220,7 +511,7 @@ class unexCreateGanttChartAction(BaseAction):
             f.close()
 
             # Create file component for job
-            job_file = os.path.basename(file_path).replace('.txt', '')
+            job_file = os.path.basename(file_path).replace('.html', '')
             component = session.create_component(
                 file_path,
                 data={'name': job_file},
